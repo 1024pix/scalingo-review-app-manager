f48f54d69995d561d3f955055d203092d4091f38
François de Metz <francois.de-metz@pix.fr>
2022-10-18 12:44:57 +0200

    Ignore 422 errors

--- test/testing.js
+++ test/testing.js
@@ -8,9 +8,21 @@ const expect = chai.expect;
 const nock = require('nock');
 nock.disableNetConnect();
 
+function catchErr(promiseFn, ctx) {
+  return async (...args) => {
+    try {
+      await promiseFn.call(ctx, ...args);
+      return 'should have thrown an error';
+    } catch (err) {
+      return err;
+    }
+  };
+}
+
 module.exports = {
   chai,
   expect,
   nock,
   sinon,
+  catchErr,
 };
